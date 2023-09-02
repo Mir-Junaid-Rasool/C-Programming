bdf27ff58769dc4d96df80fe12c730ca063a11e7
Mir Junaid Rasool <95911557+Mir-Junaid-Rasool@users.noreply.github.com>
2023-09-02 11:11:58 +0530

    first

---
+++ adjus.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int num;
+	printf("Enter no");
+	scanf("%d",&num);
+	for(int i=1;num>=i;i+=2)
+	{
+		printf("%d",i);
+	}
+	for(int i=1;num>=i;i+=2)
+	{
+		printf("%d",i);
+	}
+	return 0;
+}
